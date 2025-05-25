6802fdd42099ddfc02cbec5c3ac0837cdb934e71
Junzhi J <jjz08@qq.com>
2025-05-25 10:06:05 +0800

    update swagger config & add restart command

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -7,7 +7,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Express CMS Backend API',
+      title: 'Backend API',
       version: '1.0.0',
       description: `
 # ”食荐“者 API
